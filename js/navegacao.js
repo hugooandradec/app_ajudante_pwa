3ddcc6fc2257fdbfd6db89918e55ae648f5d213f
Victor Hugo <hugooandradec@gmail.com>
2025-07-07 01:33:58 -0300

    corrigir caminho do navegacao.js e testar integração com backend

---
+++ js/navegacao.js
@@ -0,0 +1,7 @@
+// js/navegacao.js
+
+export const URL_BACKEND = "https://script.google.com/macros/s/AKfycbxTIUjGSzKkqGd90hOkyQvKCb1mPM1mdsOLdZTvheKj7MzWxF56ve5Gvmlc-v2GjF7C/exec";
+
+export function irPara(pagina) {
+  window.location.href = `${pagina}.html`;
+}
