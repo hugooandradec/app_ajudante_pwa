b407222cff6d41910700736a23617df4c80d5688
Victor Hugo <hugooandradec@gmail.com>
2025-07-07 22:17:17 -0300

    Atualiza frontend após renomear para ajudante-app

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,7 +1,7 @@
 // js/navegacao.js
 
-export const URL_BACKEND = "https://script.google.com/macros/s/AKfycbx4Az2-O4BvllREVpipOR77M3i1dlWi1k6sbbcI8yP-0Y7zFa-YBHNl4uLgc5_b4NfT/exec";
+const URL_BACKEND = "https://ajudante-api.onrender.com";  // novo backend da Render
 
-export function irPara(pagina) {
+function irPara(pagina) {
   window.location.href = `${pagina}.html`;
 }
