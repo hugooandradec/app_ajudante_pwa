e68aa4b1231ed32b564a9ecc97d7d816f28f433e
Victor Hugo <96561669+hugooandradec@users.noreply.github.com>
2025-07-08 15:06:44 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,53 @@
-const CACHE_NAME = `ajudante-cache-v${Date.now()}`;
+const CACHE_NAME = 'ajudante-cache-v' + Date.now();
+
 const urlsToCache = [
   '/ajudante-app/',
   '/ajudante-app/index.html',
-  '/ajudante-app/cadastrarCliente.html',
-  '/ajudante-app/cadastrarMaquina.html',
-  '/ajudante-app/calculoRetencao.html',
-  '/ajudante-app/preFecho.html',
+  '/ajudante-app/login.html',
   '/ajudante-app/gerenciarClientes.html',
-  '/ajudante-app/consultarCliente.html',
+  '/ajudante-app/gerenciarMaquinas.html',
+  '/ajudante-app/cadastrarCliente.html',
   '/ajudante-app/editarCliente.html',
+  '/ajudante-app/consultarCliente.html',
   '/ajudante-app/excluirCliente.html',
-  '/ajudante-app/icon-192.png',
-  '/ajudante-app/icon-512.png',
+  '/ajudante-app/cadastrarMaquina.html',
+  '/ajudante-app/editarMaquina.html',
+  '/ajudante-app/consultarMaquina.html',
+  '/ajudante-app/excluirMaquina.html',
+  '/ajudante-app/preFecho.html',
+  '/ajudante-app/calculoRetencao.html',
   '/ajudante-app/manifest.json',
   '/ajudante-app/favicon.ico',
-  '/ajudante-app/gerenciarMaquinas',
-  '/ajudante-app/consultarMaquina',
-  '/ajudante-app/editarMaquina',
-  '/ajudante-app/excluirMaquina',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  '/ajudante-app/icon-192.png',
+  '/ajudante-app/icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    ).then(() => self.clients.claim())
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
